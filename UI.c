ae9268cab33c90c75b6ad17db6e475dd76a0273b
JasperGrant <72110751+JasperGrant@users.noreply.github.com>
2021-11-04 21:51:29 -0300

    Added customer struct

--- UI.c
+++ UI.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <io.h>
+#include <fcntl.h>
+
 #include "relativefiles.h""
 
 int main(void){
--- relativefiles.h
+++ relativefiles.h
@@ -4,7 +4,21 @@
 #include <io.h>
 #include <fcntl.h>
 
+#define NAMELEN 16
+#define BUSINESSNAMELEN 16
+#define ADRESSLEN 32
 
+struct customer{
+	long CID;
+	char lastname[NAMELEN];
+	char firstname[NAMELEN];
+	char businessname[BUSINESSNAMELEN];
+	char streetaddress[ADRESSLEN];
+	char town[NAMELEN];
+	char province[NAMELEN]
+	char postalcode[NAMELEN];
+	char telephone[]
+};
 
 
 
