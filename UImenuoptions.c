039907eda263d73d8d99fc040df446f5629c48e4
JasperGrant <72110751+JasperGrant@users.noreply.github.com>
2021-12-03 12:43:18 -0400

    Revert "Merge branch 'dev' of https://github.com/JasperGrant/StellartonSolarSystems into dev"
    This reverts commit 448bfd4f1e5c4fe1c182c8833aa33134eadcdd28, reversing
    changes made to 23294f505be865ecf87778712b8c0cd5eb2c3842.

--- UImenuoptions.c
+++ UImenuoptions.c
@@ -13,4 +13,137 @@ Authors: Jasper Grant B00829263, Rehan Khalid B00826127
 
 #include "relativefiles.h"
 
+//Initialize stores
+
+//Add to stores
+//Function brings user to menu of adding options
+int addelements(void){
+	//function selection variable
+	int input;
+	printf("Select Function\n1. Add new customers\n2. Add new suppliers\n3. Add new products\n");
+	scanf("%d", &input);
+	switch(input){
+		case 1:
+			//Add new customers
+			printf("Adding new customers...\n");
+			addnewcustomers();
+			break;
+			
+		case 2:
+			//Add new suppliers
+			printf("Adding new suppliers...\n");\
+			addnewsuppliers();
+			break;
+			
+		case 3:
+			//Add new products
+			printf("Adding new products...\n");
+			addnewproducts();
+			break;
+	}
+				
+}
+
+//Delete from stores
+//Function brings user to menu of deleting options
+int deleteelements(void){
+	//function selection variable
+	int input;
+	printf("Select Function\n1. Delete customers\n2. Delete suppliers\n3. Delete products\n4. Delete sales\n");
+	scanf("%d", &input);
+	switch(input){
+		case 1:
+			//Delete customers
+			printf("Deleting customers...\n");
+			userdeletecustomers();
+			break;
+			
+		case 2:
+			//Delete suppliers
+			printf("Deleting suppliers...\n");\
+			userdeletesuppliers();
+			break;
+			
+		case 3:
+			//Delete products
+			printf("Deleting products...\n");
+			userdeleteproducts();
+			break;
+		case 4:
+			//Delete sales
+			printf("Deleting sales...\n");
+			userdeletesales();
+			break;
+	}
+				
+}
+
+//Display Stores
+//Function brings user to menu of displaying options
+int displayelements(void){
+	//function selection variable
+	int input;
+	printf("Select Function\n1. Display customers\n2. Display suppliers\n3. Display products\n4. Display sales\n5. Display backorders");
+	scanf("%d", &input);
+	switch(input){
+		case 1:
+			//Display customers
+			printf("Displaying customers...\n");
+			readcustomers();
+			break;
+			
+		case 2:
+			//Display suppliers
+			printf("Displaying suppliers...\n");\
+			readsuppliers();
+			break;
+			
+		case 3:
+			//Display products
+			printf("Displaying products...\n");
+			readproducts();
+			break;
+		case 4:
+			//Display sales
+			printf("Displaying sales...\n");
+			readsales();
+			break;
+		case 5:
+			//Display backorders
+			printf("Displaying backorders...\n");
+			readbackorders();
+			break;
+	}
+				
+}
+
+//Change Stores
+//Function brings user to menu of changing options
+int changeelements(void){
+	//function selection variable
+	int input;
+	printf("Select Function\n1. Change element of customer\n2. Change element of supplier\n3. Change element of product\n");
+	scanf("%d", &input);
+	switch(input){
+		case 1:
+			//Change customers
+			printf("Changing customers...\n");
+			changecustomers();
+			break;
+			
+		case 2:
+			//Change suppliers
+			printf("Changing suppliers...\n");\
+			changesuppliers();
+			break;
+			
+		case 3:
+			//Change products
+			printf("Changin products...\n");
+			changeproducts();
+			break;
+		
+	}
+				
+}
 
